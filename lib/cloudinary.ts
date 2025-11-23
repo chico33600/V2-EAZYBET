47ddbdcb7f679d322492fc71e9e8f736badd03c9
chico33600 <chico33600@gmail.com>
2025-11-23 13:35:29 +0100

    Updated cloudinary.ts

--- lib/cloudinary.ts
+++ lib/cloudinary.ts
@@ -1,9 +1,15 @@
 import { v2 as cloudinary } from "cloudinary";
 
-cloudinary.config({
-  cloud_name: "ddo7omht1",
-  api_key: "522744459329385",
-  api_secret: "DdoNQUY67Ea1qrwypolo11NmgwQ",
-});
+const cloudinaryUrl = process.env.CLOUDINARY_URL;
+
+if (cloudinaryUrl) {
+  cloudinary.config(cloudinaryUrl);
+} else {
+  cloudinary.config({
+    cloud_name: "ddo7omht1",
+    api_key: "522744459329385",
+    api_secret: "DdoNQUY67Ea1qrwypolo11NmgwQ",
+  });
+}
 
 export default cloudinary;
